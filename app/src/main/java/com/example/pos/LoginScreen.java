7cc7312f08158345896542a989f5e9cd84d3cdc6
KoykoyPogi24 <randycalderon1981@gmail.com>
2025-05-24 06:07:39 +0800

    Remove edge-to-edge layout handling from Login and Sign Up screens; add activity declarations in AndroidManifest.xml

--- app/src/main/java/com/example/pos/LoginScreen.java
+++ app/src/main/java/com/example/pos/LoginScreen.java
@@ -13,12 +13,6 @@ public class LoginScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
